e544c351e3605d3ddf77e068386400423f066b15
Codeatack <codeatack1@gmail.com>
2025-06-24 17:09:30 +0300

    Update heroku_info.py
    А об этом я забыл

--- heroku/modules/heroku_info.py
+++ heroku/modules/heroku_info.py
@@ -131,6 +131,7 @@ class HerokuInfoMod(loader.Module):
             ("🐳", "<emoji document_id=5431815452437257407>🐳</emoji>"),
             ("🕶", "<emoji document_id=5407025283456835913>📱</emoji>"),
             ("🐈‍⬛", "<emoji document_id=6334750507294262724>🐈‍⬛</emoji>"),
+            ("👁️", "<emoji document_id=5350569213956210746>👁️</emoji>"),
             ("✌️", "<emoji document_id=5469986291380657759>✌️</emoji>"),
             ("💎", "<emoji document_id=5471952986970267163>💎</emoji>"),
             ("🛡", "<emoji document_id=5282731554135615450>🌩</emoji>"),
